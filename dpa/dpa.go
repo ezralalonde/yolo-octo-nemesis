d7d8ebf8fb3167802d19b6654c06b770fe61e66f
ezralalonde <ezra@usefuliftrue.com>
2013-06-09 19:29:07 -0600

    added go doc comments

--- dpa/dpa.go
+++ dpa/dpa.go
@@ -8,6 +8,8 @@ const (
 	Abundant
 )
 
+//Sum calculates the sum of all elements of parameter `xs`,
+//and returns the value.
 func Sum(xs []int) (sum int) {
 	for _, ii := range xs {
 		sum = sum + ii
@@ -15,6 +17,9 @@ func Sum(xs []int) (sum int) {
 	return sum
 }
 
+//ProperDivisors calculates all of the proper divisors of
+//parameter `xx` and returns a slice of them.
+//Proper divisors are all numbers Y such that xx % Y === 0
 func ProperDivisors(xx int) (divisors []int) {
 	for ii := 1; ii <= xx/2; ii++ {
 		if xx%ii == 0 {
@@ -24,6 +29,8 @@ func ProperDivisors(xx int) (divisors []int) {
 	return divisors
 }
 
+//IsDPA akes a number `in`, and returns whether it is
+//Deficient, Perfect, or Abundant.
 func IsDPA(in int) DPA {
 	pd := ProperDivisors
 	sum := Sum(pd(in))
@@ -35,6 +42,8 @@ func IsDPA(in int) DPA {
 	return Perfect
 }
 
+//Text takes a DPA value as a parameter, and outputs an appropriate
+//format string relating to the value of the parameter.
 func Text(in DPA) string {
 	if in == Deficient {
 		return "%v is a deficient number.\n\n"
@@ -44,6 +53,9 @@ func Text(in DPA) string {
 	return "%v is a perfect number.\n\n"
 }
 
+//Calc takes an int, and returns a piece of text regarding the
+//Abundance, Deficiency, or Perfection of a number as calculated
+//by `IsDPA` and `Text`.
 func Calc(in int) string {
     return Text(IsDPA(in))
 }
